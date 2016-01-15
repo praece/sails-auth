3429c8e493ff0cc44071cead4773fc78fd192442
Kevin OBrien <kevin.obrien@praece.com>
2016-01-15 14:57:18 -0800

    Added a configurable function for non-standard user find methods.

--- README.md
+++ README.md
@@ -7,7 +7,18 @@ Token based auth for praece sails applications. Plug and play support for auth0 
 ```javascript
 module.exports.auth = {
 	secret: 'secret!',
-	audience: 'audience'
+	audience: 'audience',
+
+	/**
+	 * Optional - a function that returns a promise that resolves to the proper user given the
+	 * supplied email param. If this function is omitted it will just search the User model for
+	 * using the email attribute.
+	 */
+	findUser: function(email) {
+		return Email.findOne({email: email})
+			.populate('user')
+			.then().get('user');
+	}
 }
 ```
 
--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,34 @@
 var passport = require('passport');
-var	BearerStrategy = require('passport-http-bearer').Strategy;
-var	jwt = require('jsonwebtoken');
+var BearerStrategy = require('passport-http-bearer').Strategy;
+var jwt = require('jsonwebtoken');
 
 passport.use(new BearerStrategy({}, function(token, done) {
-  var secret = new Buffer(sails.config.auth.secret, 'base64');
-  var audience = sails.config.auth.audience;
+  var config = sails.config.auth;
 
-  jwt.verify(token, secret, {audience: audience}, function(err, decoded) {
+  jwt.verify(token, new Buffer(config.secret, 'base64'), {audience: config.audience}, function(err, decoded) {
     if (err) {
       return done(err);
     } else if (!decoded.email) {
       return done(null, false);
     } else {
-      User.findOne({email: decoded.email}).exec(function(err, user){
-        if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        return done(null, user, {});
-      });
+      var promise;
+
+      if (config.findUser) {
+        promise = config.findUser(decoded.email);
+      } else {
+        promise = User.findOne({email: decoded.email});
+      }
+
+      promise
+        .then(function(user){
+          if (!user) { return done(null, false); }
+          return done(null, user, {});
+        })
+        .catch(done);
     }
   });
 }));
 
 module.exports.http = {
-	middleware: passport.initialize()
+  middleware: passport.initialize()
 };
