3d74da8816ccd09406b19f897b1f71c3b0c9f4d1
Kevin OBrien <kevin.obrien@praece.com>
2017-05-08 16:04:13 -0700

    Enforce specific algorithm, update all deps

--- config/passport.js
+++ config/passport.js
@@ -4,28 +4,29 @@ var jwt = require('jsonwebtoken');
 
 passport.use(new BearerStrategy({}, function(token, done) {
   var config = sails.config.auth;
+  var options = config.options || { algorithms: ['HS256'], audience: config.audience };
 
-  jwt.verify(token, new Buffer(config.secret, 'base64'), {audience: config.audience}, function(err, decoded) {
-    if (err) {
-      return done(err);
-    } else if (!decoded.email) {
-      return done(null, false);
+  jwt.verify(token, new Buffer(config.secret, 'base64'), options, function(err, decoded) {
+    // Error decoding the token
+    if (err) return done(err);
+
+    // This token has no email
+    if (!decoded.email) return done(null, false);
+
+    var promise;
+
+    if (config.findUser) {
+      promise = config.findUser(decoded.email);
     } else {
-      var promise;
-
-      if (config.findUser) {
-        promise = config.findUser(decoded.email);
-      } else {
-        promise = User.where('email', decoded.email).fetch();
-      }
-
-      return promise
-        .then(function(user){
-          if (!user) { return done(null, false); }
-          return done(null, user.toJSON(), {});
-        })
-        .catch(done);
+      promise = User.where('email', decoded.email).fetch();
     }
+
+    return promise
+      .then(function(user){
+        if (!user) { return done(null, false); }
+        return done(null, user.toJSON(), {});
+      })
+      .catch(done);
   });
 }));
 
